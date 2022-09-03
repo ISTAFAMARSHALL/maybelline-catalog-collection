bb5d4d2a42da006b53562c298411c5b78ea17c11
+ <+>
2022-09-03 13:25:11 -0400

    added db.json

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './Navigation.js/Header';
 import Navbar from './Navigation.js/Navbar';
 import Footer from './Navigation.js/Footer';
 import MakeUpComponentContainer from './MakeUpContainer.js/MakeUpComponentContainer';
-import {Switch, BrowserRouter as Router, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 import Home from './Navigation.js/Home';
 import About from './Navigation.js/About';
 import Form from './Navigation.js/Form';
@@ -12,28 +12,33 @@ import Form from './Navigation.js/Form';
 function App() {
   return (
     <div>
-      <Router>
+      
         <Header />
-        <Navbar />
-          <Switch>
-            <Route exact path="/home" >
-              <Home />
-            </Route>
 
-            <Route>
-              <Form exact path="/products/new" />
-            </Route>
-            
-            <Route>
-              <MakeUpComponentContainer exact path="/products/" />
-            </Route>
-            
-            <Route exact path="/about"  >
-              <About />
-            </Route>
-          </Switch>
+          <Navbar />
+
+            <Switch>
+
+              <Route exact path="/" >
+                <Home />
+              </Route>
+
+              <Route exact path="/products/new" >
+                <Form />
+              </Route>
+              
+              <Route exact path="/products/" >
+                <MakeUpComponentContainer />
+              </Route>
+              
+              <Route exact path="/about"  >
+                <About />
+              </Route>
+
+            </Switch>
+
         <Footer />
-      </Router>
+
     </div>
   );
 }
--- src/Navigation.js/Navbar.js
+++ src/Navigation.js/Navbar.js
@@ -2,45 +2,14 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 
 const style ={
-  width: "100%",
-  margin: "5% 0 1%",
   padding: "3em"
 }
 
 function Navbar() {
   return (
     
-    <div>
-      <NavLink 
-            activeStyle={{ 
-            fontWeight: "bolder",
-            color: "red"
-        }}  
-            style={style}
-            exact
-            to="/products/new"
-          >Add Product</NavLink>
-
-      <NavLink 
-            activeStyle={{ 
-            fontWeight: "bolder",
-            color: "red"
-        }}  
-            style={style}
-            exact
-            to="/products"
-          >Products</NavLink>
-
-        <NavLink 
-            activeStyle={{ 
-            fontWeight: "bolder",
-            color: "red"
-        }}  
-            style={style}
-            exact
-            to="/about"
-          >About</NavLink>
-
+    <div >
+      
         <NavLink 
             activeStyle={{ 
             fontWeight: "bolder",
@@ -50,7 +19,37 @@ function Navbar() {
             exact 
             to="/"
           >Home</NavLink>
-      
+
+        <NavLink 
+              activeStyle={{ 
+              fontWeight: "bolder",
+              color: "red"
+          }}  
+              style={style}
+              exact
+              to="/products/new"
+            >Add Product</NavLink>
+
+        <NavLink 
+              activeStyle={{ 
+              fontWeight: "bolder",
+              color: "red"
+          }}  
+              style={style}
+              exact
+              to="/products"
+            >Products</NavLink>
+
+          <NavLink 
+              activeStyle={{ 
+              fontWeight: "bolder",
+              color: "red"
+          }}  
+              style={style}
+              exact
+              to="/about"
+            >About</NavLink>
+
     </div>
   )
 }
