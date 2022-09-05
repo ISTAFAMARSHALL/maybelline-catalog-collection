7fc899fbee17d5dccb16543f839d2a42e793a659
+ <+>
2022-09-05 14:17:23 -0400

    updated CardDetails

--- src/MakeUpContainer.js/CardDetails.js
+++ src/MakeUpContainer.js/CardDetails.js
@@ -3,12 +3,37 @@ import { useParams } from 'react-router-dom'
 
 
 function CardDetails({products}) {
-
-  const {id } = useParams();
-  console.log(products)
   
+  parseInt()
+
+  const {id} = useParams();
+  // console.log(products)
+  // console.log(id)
+
+  const cards = products.filter((item) => { 
+    if (item.id === parseInt(id)){
+      console.log(item)
+      return item
+    }
+  })
+
+
+  const dCard = [...cards]
+
+  console.log(dCard)
+
   return (
-    <div>CardDetails</div>
+    <div key={cards.id}>
+    {/* <h1>{item.name}</h1>
+    <img src={item.image_link} alt={item.name}  ></img>
+    <p>Release Date:{item.created_at}</p>
+    <p>Prodcut Type:{item.product_type}</p>
+    <p></p>
+    <p>Rating:{item.rating}</p>
+     <p>Availble Colors:{item.product_colors}</p> 
+    <h3>Price: {item.price}</h3> */}
+</div>
+    
   )
 }
 
