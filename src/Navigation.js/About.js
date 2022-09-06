02d5e974723cf4c96332e4f99c4c1a2d702bcf0c
+ <+>
2022-09-06 13:38:09 -0400

    updated CSS

--- src/Navigation.js/About.js
+++ src/Navigation.js/About.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 function About() {
+
+
   return (
-    <div>
-      <h4>About This App</h4>
-      <div>My wife collects all types of beauty products for different occasions. Maybelline Catalog & Collection helps maintain a list of detailed information about each product.</div>
-      <div>Organize and rate your favorite beauty products within one app.</div>
+    <div id="about2">
+      <h1>About This App</h1>
+      <div id="about3" >My wife collects all types of beauty products for different occasions.</div>
+      <div id="about3" >The Maybelline Catalog & Collection helps maintain a list of detailed information about each product.</div>
+      <div id="about3">Organize and rate your favorite beauty products within one app.</div>
     </div>
   )
 }
--- src/Navigation.js/Form.js
+++ src/Navigation.js/Form.js
@@ -56,18 +56,18 @@ function Form() {
 
 
   return (
-    <div style={{margin: "20px"}}>
+    <div style={{margin: "100px"}}>
       <h2>Enter New Product Here</h2>
       <form onSubmit={handleSubmit} >
-        <label htmlFor='name'>Name:</label>
+        <label id="form" htmlFor='name'>Name:</label>
         <input onChange={(e) => setName(e.target.value)} type="text" name="name" id="name" value={name} required /> <br></br>
-        <label htmlFor='description'>Description:</label>
+        <label id="form" htmlFor='description'>Description:</label>
         <input onChange={(e) => setDescription(e.target.value)} type="text" name="description" id="description" value={description} required/> <br></br>
-        <label htmlFor='price'>Price:</label>
+        <label id="form" htmlFor='price'>Price:</label>
         <input onChange={(e) => setPrice(e.target.value)} type="text" name="price" id="price" value={price} required/> <br></br>
-        <label htmlFor='product_type'>Product Type:</label>
+        <label id="form" htmlFor='product_type'>Product Type:</label>
         <input onChange={(e) => setProduct_type(e.target.value)} type="text" name="product_type" id="product_type" value={product_type} required/> <br></br>
-        <label htmlFor='image'>Image Url:</label>
+        <label  id="form"htmlFor='image'>Image Url:</label>
         <input onChange={(e) => setImage(e.target.value)} type="text" name="image" id="image" value={image} required/> <br></br>
         <input type="submit" value="Add New Product"/>
       </form>
