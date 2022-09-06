5ebc8d35865057d9dd9e3c0c1ab3e071facb960b
+ <+>
2022-09-06 13:22:16 -0400

    added about

--- src/Navigation.js/About.js
+++ src/Navigation.js/About.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <h4>About This App</h4>
+      <div>My wife collects all types of beauty products for different occasions. Maybelline Catalog & Collection helps maintain a list of detailed information about each product.</div>
+      <div>Keep track of all you watched Anime in this smiply to use app.</div>
+    </div>
   )
 }
 
