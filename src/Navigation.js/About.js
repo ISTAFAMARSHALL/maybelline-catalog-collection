4c7ceb82eb2f28ddd658f6391688b282d57e80ac
+ <+>
2022-09-06 13:23:28 -0400

    added about

--- src/Navigation.js/About.js
+++ src/Navigation.js/About.js
@@ -5,7 +5,7 @@ function About() {
     <div>
       <h4>About This App</h4>
       <div>My wife collects all types of beauty products for different occasions. Maybelline Catalog & Collection helps maintain a list of detailed information about each product.</div>
-      <div>Keep track of all you watched Anime in this smiply to use app.</div>
+      <div>Organize and rate your favorite beauty products within one app.</div>
     </div>
   )
 }
