c3571e2bb495ed51d96a39ea339b32f6d5dd97c4
istafamarshall <istafa.a.marshall@gmail.com>
2024-07-02 02:12:17 -0400

    prep for deployment

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const [sort, setSort] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:3001/products")
+    fetch("/api/http://localhost:3001/products")
     .then(r => r.json())
     .then((data) => setproducts(data))
     .catch((error) => alert(error))
